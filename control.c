8024e4322f289b8ec227d7fd8ee4a73d7115db94
71619997a <gmarks@stuy.edu>
2016-12-08 17:26:18 -0500

    Fix to meet new hw reqs, change num back to 7

--- control.c
+++ control.c
@@ -16,16 +16,28 @@ union semun {
                                (Linux-specific) */
 };
 
+const int CREATE = 0;
+const int REMOVE = 1;
+const int VIEW = 2;
+
+printFile(int fd) {
+    // TODO
+}
 
 main(int argc, char *argv[]) {
-    int create = 1;
-    if(argc > 1 && !strcmp(argv[1], "-r")) {
-        create = 0;
-    }
-    int key = ftok("a_file", 57457);
+    int mode = VIEW;  // default
+    if(argc > 1) {
+        if(!strcmp(argv[1], "-r")) {
+            mode = REMOVE;
+        }
+        else if(!strcmp(argv[1], "-c")) {
+            mode = CREATE;
+        }
+    } 
+    int key = ftok("a_file", 7);
     int shmdesc = shmget(key, sizeof(int), IPC_CREAT | 0644);
     int semdesc = semget(key, 1, IPC_CREAT | 0644);
-    if(create) {
+    if(mode == CREATE) {
         int filedesc = creat("story", 0644);
         int *linelen;
         shmat(shmdesc, linelen, 0);
@@ -34,13 +46,17 @@ main(int argc, char *argv[]) {
         union semun msg;
         msg.val = 1;
         semctl(semdesc, 0, IPC_SET, msg);
-    } else {
+    } else if(mode == REMOVE){
         // Remove the shm and sem
         semctl(semdesc, 0, IPC_RMID);
         struct shmid_ds meta;
         shmctl(shmdesc, IPC_RMID, &meta);
         int filedesc = open("story", O_RDONLY);
-        // TODO: print story
+        printFile(filedesc);
+        close(filedesc);
+    } else {  // mode == VIEW
+        int filedesc = open("story", O_RDONLY);
+        printFile(filedesc);
         close(filedesc);
     }
     return 0;
