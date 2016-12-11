5c502750a897ab1c7ae2cfbf414a8ab3c6695199
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-11 15:58:01 -0500

    Normalize indentation to 4 spaces

--- control.c
+++ control.c
@@ -14,7 +14,7 @@ union semun {
     struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
     unsigned short  *array;  /* Array for GETALL, SETALL */
     struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                               (Linux-specific) */
+                                (Linux-specific) */
 };
 
 const int CREATE = 0;
--- writer.c
+++ writer.c
@@ -13,30 +13,30 @@
 int main() {
     if (access("story", R_OK | W_OK) == 0) {
 
-	int key = ftok("a_file", 7);
-	// sem part
-	int semdesc = semget(key, 1, IPC_CREAT | 0644);
-	struct sembuf hold = {0, -1, SEM_UNDO};  // will dec the sem, then inc on termination
-	printf("about to semop\n");
-	semop(semdesc, &hold, 1);
-	// shm part
-	int shmdesc = shmget(key, sizeof(int), 0644);
-	printf("aboutt to shmat\n");
-	int *linelen = shmat(shmdesc, 0, 0);
-	// now we can do things
-	printf("*linelen: %d\n", *linelen);
-	if(*linelen) {
-	    int filedesc = open("story", O_RDONLY);
-	    lseek(filedesc, -(*linelen), SEEK_END);
-	    char *lastline = calloc(*linelen + 1, sizeof(char));
-	    read(filedesc, lastline, *linelen + 1);
-	    lastline[*linelen] = '\0';
-	    close(filedesc);  // done with reading from file
-	    printf("Last line:\n%s", lastline);
-	} else {
-	    printf("Starting a new story\n");
-	}
+        int key = ftok("a_file", 7);
+        // sem part
+        int semdesc = semget(key, 1, IPC_CREAT | 0644);
+        struct sembuf hold = {0, -1, SEM_UNDO};  // will dec the sem, then inc on termination
+        printf("about to semop\n");
+        semop(semdesc, &hold, 1);
+        // shm part
+        int shmdesc = shmget(key, sizeof(int), 0644);
+        printf("aboutt to shmat\n");
+        int *linelen = shmat(shmdesc, 0, 0);
+        // now we can do things
+        printf("*linelen: %d\n", *linelen);
+        if(*linelen) {
+            int filedesc = open("story", O_RDONLY);
+            lseek(filedesc, -(*linelen), SEEK_END);
+            char *lastline = calloc(*linelen + 1, sizeof(char));
+            read(filedesc, lastline, *linelen + 1);
+            lastline[*linelen] = '\0';
+            close(filedesc);  // done with reading from file
+            printf("Last line:\n%s", lastline);
+        } else {
+            printf("Starting a new story\n");
+        }
     } else {
-	printf("'story' file does not exist.\n");
+        printf("'story' file does not exist.\n");
     }
 }
