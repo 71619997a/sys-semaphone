96d4d90adc42a0b620e4fdbc6f5ec9d9454497d3
Wilson Berkow <wilsonberkow@gmail.com>
2016-12-11 17:11:19 -0500

    Clean up writer.c (remove unecessary printouts and improve comments)

--- writer.c
+++ writer.c
@@ -17,24 +17,23 @@ int main() {
         // sem part
         int semdesc = semget(key, 1, IPC_CREAT | 0644);
         struct sembuf hold = {0, -1, SEM_UNDO};  // will dec the sem, then inc on termination
-        printf("about to semop\n");
+        printf("Waiting for other writers to finish...\n");
         semop(semdesc, &hold, 1);
+
         // shm part
         int shmdesc = shmget(key, sizeof(int), 0644);
-        printf("aboutt to shmat\n");
         int *linelen = shmat(shmdesc, 0, 0);
-        // now we can do things
-        printf("*linelen: %d\n", *linelen);
+
+        // As the \n is counted in *linelen, it will only be 0 when there was
+        // no previously stored line:
         if(*linelen) {
             int filedesc = open("story", O_RDONLY);
             lseek(filedesc, -(*linelen), SEEK_END);
             char *lastline = calloc(*linelen + 1, sizeof(char));
             read(filedesc, lastline, *linelen + 1);
-            printf("len of lastline: %d\n", strlen(lastline));
             close(filedesc);  // done with reading from file
             printf("Last line:\n%s", lastline);
         } else {
-            // As *linelen includes the \n, it will only be 0 when there was no previously stored line:
             printf("Starting a new story\n");
         }
         // Read a line and append it to the file:
@@ -42,9 +41,8 @@ int main() {
         fgets(line, 256, stdin);
         int fd = open("story", O_APPEND | O_WRONLY);
         write(fd, line, strlen(line));
-        printf("errno: %d. strified: %s. fd: %d\n", errno, strerror(errno), fd);
         close(fd);
-        // Update shared memory segment:
+        // Update the shared memory segment:
         *linelen = strlen(line);
         shmdt(linelen);
     } else {
